7badf57807711b470c587559853b84986f77a2a6
sauvageb <sauvageboris.pro@gmail.com>
2022-03-21 17:32:59 +0100

    feat(generations): display generations list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'PokemonsManager';
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'Pokemons Manager';
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    console.log('AppComponent On init');
+  }
+
+  ngOnDestroy(): void {
+    console.log('AppComponent On Destroy');
+  }
+
+
 }
--- src/app/components/generations/generations.component.ts
+++ src/app/components/generations/generations.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-generations',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenerationsComponent implements OnInit {
 
-  constructor() { }
+  generations: string[] = [];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.generations = [
+      "Géneration I",
+      "Géneration II",
+      "Géneration II",
+      "Géneration IV",
+      "Géneration V",
+      "Géneration VI",
+      "Géneration VII",
+    ];
   }
 
 }
