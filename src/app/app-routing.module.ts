ff33dd29d38ed5b13568039e5fb0b689ec0e2680
sauvageb <sauvageboris.pro@gmail.com>
2022-03-23 14:04:15 +0100

    feat(routing): configure routes with navbar component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {GenerationsComponent} from "./components/generations/generations.component";
+import {PokemonsComponent} from "./components/pokemons/pokemons.component";
+import {HomeComponent} from "./components/home/home.component";
+import {SigninComponent} from "./components/signin/signin.component";
+import {SignupComponent} from "./components/signup/signup.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'generations', component: GenerationsComponent},
+  {path: 'pokemons', component: PokemonsComponent},
+  {path: 'signin', component: SigninComponent},
+  {path: 'signup', component: SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { GenerationsComponent } from './components/generations/generations.component';
-import { PokemonsComponent } from './components/pokemons/pokemons.component';
-import { PokedexPipe } from './shared/pipes/pokedex.pipe';
-import { PokemonComponent } from './components/pokemon/pokemon.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {GenerationsComponent} from './components/generations/generations.component';
+import {PokemonsComponent} from './components/pokemons/pokemons.component';
+import {PokedexPipe} from './shared/pipes/pokedex.pipe';
+import {PokemonComponent} from './components/pokemon/pokemon.component';
 import {FormsModule} from "@angular/forms";
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {HomeComponent} from './components/home/home.component';
+import {SigninComponent} from './components/signin/signin.component';
+import {SignupComponent} from './components/signup/signup.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -16,15 +20,20 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     GenerationsComponent,
     PokemonsComponent,
     PokedexPipe,
-    PokemonComponent
+    PokemonComponent,
+    HomeComponent,
+    SigninComponent,
+    SignupComponent,
+    NavbarComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FormsModule,
-        FontAwesomeModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    FontAwesomeModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
